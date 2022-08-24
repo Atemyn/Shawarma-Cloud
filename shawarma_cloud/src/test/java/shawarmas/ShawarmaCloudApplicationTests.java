afd2522996986abd2a64dcb9693e068b2a01d8dd
Atemyn <atemyn@mail.ru>
2022-08-24 16:49:15 +0700

    Initial Git Commit

---
+++ shawarma_cloud/src/test/java/shawarmas/ShawarmaCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package shawarmas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShawarmaCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
