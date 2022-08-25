4cc3a584cc59195b2f3d2c643691f15a1d70f58f
Atemyn <atemyn@mail.ru>
2022-08-25 22:16:27 +0700

    Добавлена валидация полей для Shawarma

--- shawarma_cloud/src/main/java/shawarmas/model/Shawarma.java
+++ shawarma_cloud/src/main/java/shawarmas/model/Shawarma.java
@@ -1,10 +1,19 @@
 package shawarmas.model;
 
 import java.util.List;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
+
 
 @Data
 public class Shawarma {
+	@NotNull
+	@Size(min=5, message="Name must be at least 5 characters long")
 	private String name;
+	@NotNull
+	@Size(min = 1, message="Shawarma should have at least 1 ingredient")
 	private List<Ingredient> ingredients;
 }
